55871b993269cfd48c579828f5ad01f0f189fd6e
Pavan M G <pavanmggp@gmail.com>
2026-03-17 20:34:39 +0530

    Update from Github copilot

--- js/loader.js
+++ js/loader.js
@@ -1,16 +1,6 @@
-/* Add a event listener for Loading... */
-window.addEventListener("load", () => {
-    const loader = document.querySelector(".loader");
-    /* Add Class to hide Loader... */
-    loader.classList.add("loader--hidden");
-    loader.addEventListener("transitionend", () => {
-    /* Remove Loaded Class once Page is Loaded... */
-      loader.classList.remove("loader");
-    });
-    /* Setting the border height of content desplayed */
-    var content_div = document.getElementById("Home")
-    var left_col_div = document.getElementById("left_col")
-    if(content_div.clientHeight < left_col_div.clientHeight - 20) {
-    content_div.style.height = left_col_div.clientHeight - 20 +'px';
-    }
-  });
+﻿window.addEventListener('load', ()=>{
+  const loader = document.querySelector('.loader');
+  if (!loader) return;
+  loader.classList.add('loader--hidden');
+  loader.addEventListener('transitionend', ()=>{ loader.remove(); }, { once: true });
+});
